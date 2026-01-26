5ebef3fc91558f9cd9fecb8f02189f3296209763
Kiefer Ebanks <kieferebanks@gmail.com>
2026-01-26 18:00:21 -0500

    added time to homepage

--- django/hw/views.py
+++ django/hw/views.py
@@ -1,15 +1,17 @@
 # file: django/hw/views.py
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+import time # This is to show its a static website
 
 # Create your views here.
 
 def home(request: HttpRequest) -> HttpResponse:
     '''A function that responds to the home request'''
 
-    response_text = '''
+    response_text = f'''
     <html>
         <h1>Hello, World!</h1>
+        The current time is: { time.ctime() }.
     </html>
     '''
 
