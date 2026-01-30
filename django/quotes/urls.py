8dc36813015898e4ea13ea6fc16f252c04f5147d
Kiefer Ebanks <kieferebanks@gmail.com>
2026-01-30 13:38:18 -0500

    added comments to all files i created

--- django/quotes/urls.py
+++ django/quotes/urls.py
@@ -1,5 +1,5 @@
 # File: urls.py
-# Author: Kiefer Ebanks (kebanks@bu.edu), 1/27/20026
+# Author: Kiefer Ebanks (kebanks@bu.edu), 1/27/2026
 # Description: The URL file for the quotes app
 # Creating the URLs for the quotes app
 
@@ -8,10 +8,10 @@ from django.urls import path
 from django.conf import settings
 from . import views
  
-# Creating my URLs. So far just the home page.
+# Creating my URL paths.
 urlpatterns = [ 
-    path(r'', views.home, name="home"),
-    path(r'quote/', views.quote, name="quote"),
-    path(r'show_all/', views.show_all, name="show_all"),
-    path(r'about/', views.about, name="about"),
+    path(r'', views.home, name="home"), # the default home page
+    path(r'quote/', views.quote, name="quote"), # page that shows a random quote and image
+    path(r'show_all/', views.show_all, name="show_all"), # page that shows all Tom Brady quotes and images
+    path(r'about/', views.about, name="about"), # page that shows basic information about Tom Brady and myself
 ]
